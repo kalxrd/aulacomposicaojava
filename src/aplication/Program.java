d8b82ae27c6baa3f3fdbb4d161817b674864f69a
Karoline Marques <marqueskarol682@gmail.com>
2023-11-10 13:51:21 -0300

    Exercício de composição Java

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,17 +1,19 @@
 package aplication;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Department;
+import entities.HourContract;
 import entities.Worker;
 import entities.enums.WorkerLevel;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException{
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
@@ -32,14 +34,29 @@ public class Program {
 		System.out.println("How many contracts to this worker? ");
 		int n = sc.nextInt();
 		
-		for(int i = 1; i<n; i++) {
+		for(int i = 1; i<=n; i++) {
 			System.out.println("Enter contract #" + i + "data:");
 			System.out.print("Date (DD/MM/YYYY)");
 			Date contractDate = sdf.parse(sc.next());
-			
+			System.out.println("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+;			System.out.println("Duration (hours): ");
+			int hours =sc.nextInt();
+			HourContract contract = new HourContract (contractDate, valuePerHour, hours);
+		
 		}
 		
+		System.out.println();
+		System.out.println("Enter month and year to calculate income (MM/YYYY)");
+		String monthAndYear = sc.next();
+		int month = Integer.parseInt(monthAndYear.substring(0,2));
+		int year = Integer.parseInt(monthAndYear.substring(3));
 		
+		System.out.println("Name: " +  worker.getName());
+		System.out.println("Department: " + worker.getDepartment().getName());
+		System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year,month)));
+		
+		sc.close();
 	
 	
 	}
